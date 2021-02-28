ae0162ec5e6dd177b4c550b4a40edba6cadcdd6b
hanggegreat <hanggegreat@vip.qq.com>
2021-02-28 23:56:34 +0800

    fix import bug

--- raft/common.go
+++ raft/common.go
@@ -1,52 +1,45 @@
 package raft
 
 import (
-	"log"
 	"math/rand"
 	"time"
 )
 
-const VoteNil = -1
-
-type PeerState int
-
 const (
-	Follower = iota
-	Candidate
-	Leader
+	Follower            = "Follower"
+	Candidate           = "Candidate"
+	Leader              = "Leader"
+	VoteNil             = -1
+	HeartbeatInterval   = 100 * time.Millisecond
+	RpcCallTimeout      = 800 * time.Millisecond
+	MinElectionDuration = 300
+	MaxElectionDuration = 450
 )
-
-const RpcCallTimeout = time.Second
-const HeartbeatInterval = 100 * time.Millisecond
-
-const MinElectionDuration = 1100
-const MaxElectionDuration = 1400
-
-// Debugging
-const Debug = true
-
-func DInfo(format string, a ...interface{}) {
-	log.Printf(format, a...)
-	return
-}
-
-func DPrintf(format string, a ...interface{}) {
-	if Debug {
-		log.Printf(format, a...)
-	}
-	return
-}
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-	log.SetFlags(log.Lmicroseconds)
 }
 
-// Generate int with range in [from, to)
+// range in [from, to)
 func RandomRange(from, to int) int {
 	return rand.Intn(to-from) + from
 }
 
-func genRandomRangeDuration() time.Duration {
+//获取随机时间，用于选举
+func electionTimeout() time.Duration {
 	return time.Duration(RandomRange(MinElectionDuration, MaxElectionDuration)) * time.Millisecond
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
 }
--- raft/raft.go
+++ raft/raft.go
@@ -1,13 +1,30 @@
 package raft
 
 import (
+	"bytes"
+	"distributed-project/labgob"
 	"distributed-project/labrpc"
-	"fmt"
 	"sync"
-	"sync/atomic"
-	//"6.824/labgob"
+	"time"
 )
 
+type ApplyMsg struct {
+	// true 为正常信息交付，false 为快照
+	CommandValid bool
+	Command      interface{}
+	CommandIndex int
+	// 交付信息时 raft 的 term
+	CommitTerm int
+	// 交付信息时 raft 的角色
+	Role string
+
+	SnapshotValid bool
+	Snapshot      []byte
+	SnapshotTerm  int
+	SnapshotIndex int
+}
+
+// 每一个 raft peer 都叫 server，分为 Leader, Candidate, Follower 三种角色
 type Raft struct {
 	// 通过锁来共享结点状态
 	mu *sync.Mutex
@@ -17,116 +34,218 @@ type Raft struct {
 	persister *Persister
 	// 当前 peer 在 peers[] 中的 index
 	me int
-	// set by Kill()
-	dead int32
-
-	// current term
-	curTerm  int
+	// Leader, Candidate or Follower
+	role string
+	// 结点当前 Term
+	currentTerm int
+	// 所投的 server index, 初始化为 VoteNil(-1), 没有投票给其他人
 	votedFor int
-	// log entries; each entry contains command for state machine, and term when entry was received by leader(first index is 1)
-	logs []LogEntry
+	// 保存执行的命令，index 从 1 开始
+	logs []Entries
+	// 最后一个提交的日志 index
+	commitIndex int
+	// 最后一个应用到状态机的日志 index, 从 0 开始
+	lastApplied int
 
-	// 结点状态(Follower, Candidate, Leader)
-	state PeerState
-	// 选举超时器
-	timer *Timer
-	// every Raft peer has a condition, use for trigger AppendEntries RPC
-	syncConds []*sync.Cond
+	// Leader 才会初始化，每一次 election 后重置
+	// 保存发给每个 Follower 的下一条日志 index，初始为 Leader 的最后一条日志 index + 1
+	nextIndex []int
+	// 保存每个 Follower 已经接受到的最后一条日志的 index
+	matchIndex []int
 
-	// Your data here (2A, 2B, 2C).
-	// Look at the paper's Figure 2 for a description of what
-	// state a Raft server must maintain.
+	// 接收到心跳后写入
+	appendCh chan bool
+	// 投票后写入
+	voteCh chan bool
+	// 结束实例写入
+	exitCh chan bool
+	// candidate 竞选 leader 写入
+	leaderCh chan bool
+
+	// commit log 后写入数据
+	applyCh chan ApplyMsg
+
+	// 现存快照对应的最后一个日志 index
+	lastIncludedIndex int
+	// 现存快照对应的最后一个日志所属 Term
+	lastIncludedTerm int
 }
 
 func (rf *Raft) GetState() (int, bool) {
-	return rf.curTerm, rf.state == Leader
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	return rf.currentTerm, rf.role == Leader
 }
 
-func (rf *Raft) isLeader() bool {
-	return rf.state == Leader
+func (rf *Raft) persist() {
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+	e.Encode(rf.currentTerm)
+	e.Encode(rf.votedFor)
+	e.Encode(rf.logs)
+	e.Encode(rf.lastIncludedIndex)
+	e.Encode(rf.lastIncludedTerm)
+	data := w.Bytes()
+	rf.persister.SaveRaftState(data)
+	InfoRaft.Printf("Raft:%d term:%d | Persist data! Size:%5v logs:%4v\n", rf.me, rf.currentTerm, len(data), len(rf.logs)-1)
 }
 
-func (rf *Raft) String() string {
-	return fmt.Sprintf("serverId: %d, term: %d, voteFor: %d, state: %d, dead: %d", rf.me, rf.curTerm, rf.votedFor, rf.state, rf.dead)
-}
-
-// 成为 Term 为 term，Leader 为 voteFor 的 Follower
-func (rf *Raft) back2Follower(term, voteFor int) {
-	rf.state = Follower
-	rf.curTerm = term
-	rf.votedFor = voteFor
-	rf.resetElectTimer()
-}
-
-// 创建并初始化 Raft
-func Make(peers []*labrpc.ClientEnd, me int, persister *Persister, applyCh chan ApplyMsg) *Raft {
-	rf := &Raft{}
-	rf.mu = &sync.Mutex{}
-	rf.peers = peers
-	rf.persister = persister
-	rf.me = me
-
-	rf.curTerm = 0
-	rf.votedFor = VoteNil
-	rf.state = Follower
-	rf.timer = NewElectTimer()
-	rf.dead = 0
-	rf.logs = make([]LogEntry, 0)
-	rf.syncConds = make([]*sync.Cond, len(peers))
-	for i := range peers {
-		rf.syncConds[i] = sync.NewCond(rf.mu)
+func (rf *Raft) readPersist(data []byte) {
+	if data == nil || len(data) < 1 {
+		return
 	}
+	r := bytes.NewBuffer(data)
+	d := labgob.NewDecoder(r)
 
-	// initialize from state persisted before a crash
-	rf.readPersist(persister.ReadRaftState())
+	var term int
+	var votedFor int
+	var logs []Entries
+	var lastIncludedIndex int
+	var lastIncludedTerm int
 
-	// start ticker goroutine to start elections
-	go rf.ticker()
-
-	return rf
+	if d.Decode(&term) != nil || d.Decode(&votedFor) != nil || d.Decode(&logs) != nil || d.Decode(&lastIncludedIndex) != nil || d.Decode(&lastIncludedTerm) != nil {
+		InfoRaft.Printf("Raft:%d term:%d | Failed to read persist data!\n", rf.me, rf.currentTerm)
+	} else {
+		rf.currentTerm = term
+		rf.votedFor = votedFor
+		rf.logs = logs
+		rf.lastIncludedIndex = lastIncludedIndex
+		rf.lastIncludedTerm = lastIncludedTerm
+		rf.lastApplied = rf.lastIncludedIndex
+		rf.commitIndex = rf.lastIncludedIndex
+		InfoRaft.Printf("Raft:%d term:%d | Read persist data{%d bytes} successful! VotedFor:%d len(Logs):%d\n", rf.me, rf.currentTerm, len(data), rf.votedFor, len(rf.logs))
+	}
 }
 
-//
-// the service using Raft (e.g. a k/v server) wants to start
-// agreement on the next command to be appended to Raft's log. if this
-// server isn't the leader, returns false. otherwise start the
-// agreement and return immediately. there is no guarantee that this
-// command will ever be committed to the Raft log, since the leader
-// may fail or lose an election. even if the Raft instance has been killed,
-// this function should return gracefully.
-//
-// the first return value is the index that the command will appear at
-// if it's ever committed. the second return value is the current
-// term. the third return value is true if this server believes it is
-// the leader.
-//
+// 角色转换
+func (rf *Raft) convertRole(role string) {
+	defer rf.persist()
+	switch role {
+	case Follower:
+		rf.votedFor = VoteNil
+		WarnRaft.Printf("Raft:%d term:%d | %s convert role to Follower!\n", rf.me, rf.currentTerm, rf.role)
+	case Candidate:
+		rf.currentTerm = rf.currentTerm + 1
+		rf.votedFor = rf.me
+		WarnRaft.Printf("Raft:%d term:%d | %s convert role to Candidate!\n", rf.me, rf.currentTerm, rf.role)
+	case Leader:
+		WarnRaft.Printf("Raft:%d term:%d | %s convert role to Leader!\n", rf.me, rf.currentTerm, rf.role)
+		for i := range rf.peers {
+			if i == rf.me {
+				continue
+			}
+			rf.nextIndex[i] = len(rf.logs) + rf.lastIncludedIndex
+			rf.matchIndex[i] = 0
+		}
+	}
+	rf.role = role
+}
+
+// 检查 server 的状态是否与之前一致
+func (rf *Raft) checkState(role string, term int) bool {
+	return rf.role == role && rf.currentTerm == term
+}
+
+// 客户端执行命令
 func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	index := -1
-	term := -1
-	isLeader := true
 
-	// Your code here (2B).
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	term := rf.currentTerm
+	isLeader := rf.role == Leader
+
+	if isLeader {
+		index = rf.getLastLogIndex() + 1
+		rf.logs = append(rf.logs, Entries{rf.currentTerm, index, command})
+		InfoRaft.Printf("Raft:%d term:%d | Leader receive a new command:%v cmdIndex:%v commitIndex:%v\n", rf.me, rf.currentTerm, command, index, rf.commitIndex)
+		rf.persist()
+	}
 
 	return index, term, isLeader
 }
 
-//
-// the tester doesn't halt goroutines created by Raft after each test,
-// but it does call the Kill() method. your code can use killed() to
-// check whether Kill() has been called. the use of atomic avoids the
-// need for a lock.
-//
-// the issue is that long-running goroutines use memory and may chew
-// up CPU time, perhaps causing later tests to fail and generating
-// confusing debug output. any goroutine with a long-running loop
-// should call killed() to check whether it should stop.
-//
 func (rf *Raft) Kill() {
-	atomic.StoreInt32(&rf.dead, 1)
-	// Your code here, if desired.
+	rf.exitCh <- true
 }
 
-func (rf *Raft) killed() bool {
-	z := atomic.LoadInt32(&rf.dead)
-	return z == 1
+func (rf *Raft) ticker() {
+	for {
+		select {
+		case <-rf.exitCh:
+			return
+		default:
+		}
+		rf.mu.Lock()
+		role := rf.role
+		rf.mu.Unlock()
+
+		switch role {
+		case Follower:
+			select {
+			case <-rf.voteCh:
+			case <-rf.appendCh:
+			case <-time.After(electionTimeout()):
+				rf.mu.Lock()
+				rf.convertRole(Candidate)
+				rf.mu.Unlock()
+			}
+		case Candidate:
+			go rf.leaderElection()
+
+			select {
+			case <-rf.voteCh:
+			case <-rf.appendCh:
+			case <-rf.leaderCh:
+			case <-time.After(electionTimeout()):
+				rf.mu.Lock()
+				rf.convertRole(Candidate)
+				rf.mu.Unlock()
+			}
+		case Leader:
+			rf.broadcastEntries()
+			<-time.After(HeartbeatInterval)
+		}
+	}
+}
+
+func Make(peers []*labrpc.ClientEnd, me int, persister *Persister, applyCh chan ApplyMsg) *Raft {
+	rf := &Raft{}
+	rf.peers = peers
+	rf.persister = persister
+	rf.me = me
+	rf.mu = &sync.Mutex{}
+	rf.role = Follower
+	rf.currentTerm = 0
+	rf.votedFor = VoteNil
+
+
+	// 第 0 个占位
+	rf.logs = make([]Entries, 1)
+
+	rf.commitIndex = 0
+	rf.lastApplied = 0
+	rf.nextIndex = make([]int, len(peers))
+	rf.matchIndex = make([]int, len(peers))
+
+	// 容量为 1 防止写入阻塞
+	rf.appendCh = make(chan bool, 1)
+	rf.voteCh = make(chan bool, 1)
+	rf.exitCh = make(chan bool, 1)
+	rf.leaderCh = make(chan bool, 1)
+
+	rf.applyCh = applyCh
+
+	rf.lastIncludedIndex = 0
+	rf.lastIncludedTerm = 0
+
+	rf.readPersist(persister.ReadRaftState())
+	rf.logs[0] = Entries{rf.lastIncludedTerm, rf.lastIncludedTerm, -1}
+
+	InfoRaft.Printf("Create a new Raft:[%d]! term:[%d]! Log length:[%d]\n", rf.me, rf.currentTerm, rf.getLastLogIndex())
+
+	go rf.ticker()
+
+	return rf
 }
